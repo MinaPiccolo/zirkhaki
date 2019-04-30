6b99c81565b18088222b6b5b511be38b46ab57cd
MinaPiccolo <mina.g1363@yahoo.com>
2019-04-30 08:00:08 +0000

    جنریت کردن تعدادی از آبجکت ها و دراپ آیتم برداشتن آیتم ها و گذاشتن در اینونتوری
    T4752

---
+++ Assets/scripts/itemObj.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class itemObj : MonoBehaviour
+{
+    public int index;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => { onClickGameObj(); });
+    }
+
+    public void collectItems()
+    {
+    }
+    void onClickGameObj()
+    {
+        player.Instance.putToInventory(index);
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public int[] inventory;
+    static player _instance;
+    public static player Instance
+    {
+        get { return _instance; }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+    void Start()
+    {
+        
+    }
+    public void putToInventory(int indexItem)
+    {
+        int findEmptySlot = checkInventoryFull();
+        if (findEmptySlot !=-1)
+        {
+            inventory[findEmptySlot] = indexItem;
+        }
+    }
+    int checkInventoryFull()
+    {
+        for (int i = 0; i < inventory.Length; i++)
+            if (inventory[i] == 0)
+                return i;
+
+        return -1;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
